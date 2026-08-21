39c635026b6e404975c4afdf41ae3d75773ffc98
Kage Jinsei <dadinho_123@outlook.com>
2026-08-21 16:18:53 -0300

    adicionando searching

---
+++ searching.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void comparar(char sequencia[], char vocabulario[], int tamanho);
+
+void vocabularios(char vocabulario[]);
+
+int main() {
+  char *strInputVoc = malloc(sizeof(char));
+  char *strInput = malloc(sizeof(char));
+  int length, op;
+
+  printf("1 - vocabulario padrao\n");
+  printf("2 - adicionar vocabulario\n");
+
+  scanf("%d", &op);
+
+  if (op == 1) {
+    printf("\nVocabulario definido: [a-z]\n");
+    strInputVoc = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    printf("Digite um vocabulario: ");
+    scanf("%s", strInputVoc);
+  }
+
+  printf("\nDigite uma palavra: ");
+  scanf("%s", strInput);
+
+  for (length = 0; strInput[length]; length++)
+    ;
+
+  comparar(strInput, strInputVoc, length);
+
+  return 0;
+}
+
+void comparar(char sequencia[], char vocabulario[], int tamanho) {
+  char seqcmp[tamanho];
+  int c;
+
+  for (int i = 0; i < sequencia[i]; i++) {
+    for (int j = 0; i != tamanho; j++) {
+      if (sequencia[i] == vocabulario[j]) {
+        seqcmp[i] = sequencia[i];
+        j = 0;
+        break;
+      }
+    }
+  }
+    
+  seqcmp[strlen(seqcmp)] = '\0';
+
+  c = strcmp(sequencia, seqcmp);
+
+  if (c == 0) {
+    printf("Palavra aceita!\n");
+  } else {
+    printf("Palavra rejeitada!\n");
+  }
+
+  
+  for (int i = 0; i < seqcmp[i]; i++) {
+    putchar(seqcmp[i]);
+  }
+  
+
+  putchar('\n');
+
+  printf("%d\n", c);
+}
+
+void vocabularios(char vocabulario[]);
